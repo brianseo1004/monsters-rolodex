05e6167150b333276f658b7fa589d9a013a6eb8f
Brian <brianseo1004@gmail.com>
2020-03-21 12:17:56 -0400

    Updates

---
+++ precache-manifest.8693b44aff1aedd033d17b6b6264e0d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003d73ae7262fd547f78f494e244b3e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "251f4869e4dc253fbe6e",
+    "url": "/monsters-rolodex/static/css/main.fcbda1a7.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251f4869e4dc253fbe6e",
+    "url": "/monsters-rolodex/static/js/main.b4de1ff1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
