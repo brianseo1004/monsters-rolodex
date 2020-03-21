b3452b8fb0eac5968b0101779ed1eacfe508b9bc
Brian <brianseo1004@gmail.com>
2020-03-21 12:09:53 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import logo from "./logo.svg";
 import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asc1"
-        },
-        {
-          name: "Dracula",
-          id: "asc2"
-        },
-        {
-          name: "Zombie",
-          id: "asc3"
-        }
-      ]
+      monsters: [],
+      searchField: ""
     };
+
+    //Arrow function automatically binds this to the method.
+    //this.handleChange = this.handleChange.bind(this);
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
 
   //As state changes, render gets run again
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
